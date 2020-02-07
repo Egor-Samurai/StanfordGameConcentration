c5a7823a93423244a28572be7fab7b043d0095a5
Egor Gurba <teninworkprime@gmail.com>
2020-02-07 12:07:16 +0200

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Егор on 07.02.2020.
+//  Copyright © 2020 Egor Gurba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
